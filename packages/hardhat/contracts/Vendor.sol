11dd368b9be8fdf874edcaec84252d82653bb388
HarrietLy <harriet.ly.trang@gmail.com>
2025-07-30 15:49:20 +0700

    fix: remove the 10**18 scale

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -12,17 +12,16 @@ contract Vendor is Ownable, ReentrancyGuard{
     YourToken public yourToken;
     uint public constant tokensPerEth = 100;
 
-    constructor(address tokenAddress) Ownable (msg.sender) {
+    constructor(address tokenAddress) Ownable (msg.sender){
         yourToken = YourToken(tokenAddress);
     }
 
     // create a payable buyTokens() function:
-    function buyTokens() payable external {
+    function buyTokens() payable external nonReentrant{
         // msg.sender is the buyer of the tokens, they will use ETH amt sent in msg.data to buy tokens
         uint256 ethInput = msg.value;
-        uint256 tokensVendorDispense = tokensPerEth * ethInput * 10 ** 18;
-        // check if vendor has enough
-        require(tokensVendorDispense <= yourToken.balanceOf(address(this)), " insuffcient tokens to dispense");
+        require(ethInput > 0, "eth input cannot be <=0 if buying tokens");
+        uint256 tokensVendorDispense = tokensPerEth * ethInput;
         // transfer token to user
         yourToken.transfer(msg.sender, tokensVendorDispense); //transfer token from Vendor (already transfer all 1000 tokens to vendor in deploy scripts) to msg sender 
         // user can pay eth to this vendor contract because this function is payable, hence can receive eth
@@ -40,17 +39,17 @@ contract Vendor is Ownable, ReentrancyGuard{
     //  create a sellTokens(uint256 _amount) function, user sell token to vendor
     function sellTokens(uint256 tokenInput) external {
         require( tokenInput > 0, "token input must be positive");
-        uint ethVendorToDispense = tokenInput / tokensPerEth * 10 ** 18; 
+        uint ethVendorToDispense = tokenInput / tokensPerEth; 
         //check if vendor has enough ETH to return to caller
         require(ethVendorToDispense <= address(this).balance, " insufficient ETH to dispense");
 
         //vendor transfer token from user back to vendor , can only succeed if user already approved vendor as a spender to transfer tokens on their behalf
-        yourToken.transferFrom( msg.sender, address(this), tokenInput * 10 ** 18);
+        yourToken.transferFrom( msg.sender, address(this), tokenInput);
         
         // transfer eth to user
         (bool sent, ) = msg.sender.call{value: ethVendorToDispense}("");
         require(sent, " ETH transfer failed");
-
+        emit SellTokens(msg.sender, ethVendorToDispense, tokenInput);
 
     }
 }
